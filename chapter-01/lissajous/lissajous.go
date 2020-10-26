20496494dee0b809e2347dfbd205092a01960e4d
Chase Peterson <cpeterson.chase@gmail.com>
2020-10-26 09:52:36 -0600

    Messing with images and gifs

---
+++ chapter-01/lissajous/lissajous.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+    "image"
+    "image/color"
+    "image/gif"
+    "io"
+    "math"
+    "math/rand"
+    "os"
+)
+
+
+// var palette = []color.Color{color.Black,
+//     color.RGBA{0x00, 0x80, 0x00, 0xff}, // Green
+//     color.RGBA{0xff, 0xd7, 0x00, 0xff}, // Gold
+//     color.RGBA{0x00, 0x00, 0x80, 0xff}, // Navy
+//     color.RGBA{0x4b, 0x00, 0x82, 0xff}, // Indigo
+//     color.RGBA{0x40, 0xe0, 0xd0, 0xff}, // Turquoise
+//     // color.White,
+// }
+
+var palette = []color.Color{color.Black}
+
+const (
+    blackIndex = 0
+    greenIndex = 1
+    whiteIndex = 2
+)
+
+func main() {
+    lissajous(os.Stdout)
+}
+
+func lissajous(out io.Writer) {
+    const (
+        cycles = 5   // number of complete x oscillator revolutions
+        res = 0.001  // angular resolution
+        size = 100   // image canvas covers [-size..+size]
+        nframes = 64 // number of animation frames
+        delay = 8    // delay between frames in 10ms units
+    )
+
+    for i := 0; i < nframes; i++ {
+        palette = append(palette, color.RGBA{0x00, uint8(i), 0x00, 0xff}) // Fades out green
+    }
+
+    freq := rand.Float64() * 3.0 // relative frequency of y oscillator
+    anim := gif.GIF{LoopCount: nframes}
+    phase := 0.0 // phase difference
+    for i := 0; i < nframes; i++ {
+        rect := image.Rect(0, 0, 2*size+1, 2*size+1)
+        img := image.NewPaletted(rect, palette)
+        for t := 0.0; t < cycles*2*math.Pi; t += res {
+            x := math.Sin(t)
+            y := math.Sin(t*freq + phase)
+            img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5), uint8(i))
+        }
+        phase += 0.1
+        anim.Delay = append(anim.Delay, delay)
+        anim.Image = append(anim.Image, img)
+    }
+    gif.EncodeAll(out, &anim) // NOTE: ignoring encoding errors
+}
